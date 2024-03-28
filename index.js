cd6a807674ebac7bf6d8d05d023a0d41017db7ee
MBichajlo <maciej.bichajlo.mb@gmail.com>
2024-03-28 20:29:59 +0100

    Color scheme and layout made, basic get requests handling

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/random-post", (req, res) => {
+  res.redirect("/post");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post.ejs");
+});
+
+app.get("/new-post", (req, res) => {
+  res.render("newPost.ejs");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
