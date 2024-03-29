277f8fc85b8c80771b195c2fc309572f763a1f0f
MBichajlo <maciej.bichajlo.mb@gmail.com>
2024-03-29 12:07:51 +0100

    Added post viewing and editing functions, worked around html form not having a put request as a method, need to write put request handling to finish post editing part

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import fs from "fs";
+import path from "path";
 
 const port = 3000;
 const app = express();
@@ -16,8 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   fs.readdir("public/assets/posts", (err, files) => {
     if (err) throw err;
+    var filteredFiles = files.filter((e) => {
+      return path.extname(e).toLowerCase() === ".txt";
+    });
     var newFiles = [];
-    files.forEach((element) => {
+    filteredFiles.forEach((element) => {
       newFiles.push(element.slice(0, -4));
     });
     console.log(newFiles);
@@ -33,20 +37,40 @@ app.get("/random-post", (req, res) => {
 
 app.get("/post", (req, res) => {
   const arr1 = req.url.split("?");
-  console.log(arr1[1].replace(/%20/g, " "));
+  const postName = arr1[1].replace(/%20/g, " ");
   fs.readFile(
-    "public/assets/posts/" + arr1[1].replace(/%20/g, " ") + ".txt",
+    "public/assets/posts/" + postName + ".txt",
     "utf-8",
     (err, data) => {
       if (err) throw err;
-      console.log(data);
+      res.render("post.ejs", {
+        postName: postName,
+        postText: data,
+      });
     }
   );
-  res.render("post.ejs");
 });
 
-app.get("/new-post", (req, res) => {
-  res.render("newPost.ejs");
+app.get("/editor", (req, res) => {
+  const link = req.url.split("?");
+
+  if (link[1]) {
+    const postName = link[1].replace(/%20/g, " ");
+    fs.readFile(
+      "public/assets/posts/" + postName + ".txt",
+      "utf-8",
+      (err, data) => {
+        if (err) throw err;
+
+        res.render("newPost.ejs", {
+          postName: postName,
+          postText: data,
+        });
+      }
+    );
+  } else {
+    res.render("newPost.ejs");
+  }
 });
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
@@ -55,17 +79,21 @@ app.get("/contact", (req, res) => {
 // Posts
 
 app.post("/save", (req, res) => {
-  let postTitle = req.body["title"];
-  let postText = req.body["postText"];
-  fs.writeFile(
-    `public/assets/posts/${postTitle.trimEnd()}.txt`,
-    postText,
-    "utf-8",
-    (err) => {
-      if (err) throw err;
-      res.render("postSaved.ejs");
-    }
-  );
+  if (req.body["_METHOD"] === "POST") {
+    let postTitle = req.body["title"];
+    let postText = req.body["postText"];
+    fs.writeFile(
+      `public/assets/posts/${postTitle.trimEnd()}.txt`,
+      postText,
+      "utf-8",
+      (err) => {
+        if (err) throw err;
+        res.render("postSaved.ejs");
+      }
+    );
+  } else if (req.body["_METHOD"] === "PUT") {
+    res.send("<h1> put request</h1>");
+  }
 });
 
 app.listen(port, () => {
